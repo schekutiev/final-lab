d49e5b99401e1c463da3dbfbe33369b742ca531d
schekutiev <eugene.schekutiev@gmail.com>
2021-10-25 20:16:38 -0700

    Makemigrations/Migrate. Create/Test superuser.

--- myproject/settings.py
+++ myproject/settings.py
@@ -128,6 +128,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
 MEDIA_URL = '/media/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
