57f0f9c64c514e4cbdfe4de874e3448780e05d5f
schekutiev <eugene.schekutiev@gmail.com>
2021-10-25 21:22:55 -0700

    Add __str__ method to Lesson model. Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,41 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
-
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import Course, Instructor, Learner, Lesson, Enrollment, Question, Choice, Submission
 
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
+class QuestionInline(admin.StackedInline):
+    model = Question.courses.through
+    extra = 2
 
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+class ChoiceInline(admin.TabularInline):
+    model = Choice.questions.through
+    extra = 2
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('text', 'grade', 'created_at')
+    list_filter = ['created_at']
+    search_fields = ['text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['text', 'is_correct']
+
 
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Lesson)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Enrollment)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Submission)
