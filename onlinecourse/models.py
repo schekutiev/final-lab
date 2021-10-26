5e20198e91a0cc33a4f9dde98a268c995e9e57c2
schekutiev <eugene.schekutiev@gmail.com>
2021-10-25 19:44:42 -0700

    Add models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -48,14 +48,14 @@ class Learner(models.Model):
     social_link = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.user.username + "," + \
+        return self.user.username + " is " + \
                self.occupation
 
 
 # Course model
 class Course(models.Model):
     name = models.CharField(null=False, max_length=30, default='online course')
-    image = models.ImageField(upload_to='course_images/')
+    image = models.ImageField(upload_to='course_images/', default='default.jpg')
     description = models.CharField(max_length=1000)
     pub_date = models.DateField(null=True)
     instructors = models.ManyToManyField(Instructor)
@@ -94,6 +94,9 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+    def __str__(self) -> str:
+        return str(self.user.username)
+
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
@@ -101,19 +104,24 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
+class Question(models.Model):
+    text = models.CharField(max_length=100)
+    grade = models.IntegerField(default=1)
+    courses = models.ManyToManyField(Course)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    ##### You have aswer 100% right to get the grade #####
+    def is_get_score(self, selected_ids):
+        correct_choices_count = self.choice_set.filter(is_correct=True).count()
+        selected_choices_count = self.choice_set.filter(
+            id__in=selected_ids).count()
+        correct_selected_choices_count = self.choice_set.filter(
+            is_correct=True, id__in=selected_ids).count()
+        return correct_choices_count == selected_choices_count == correct_selected_choices_count
+
+    def __str__(self) -> str:
+        return self.text
 
 
 #  <HINT> Create a Choice Model with:
@@ -122,13 +130,19 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    questions = models.ManyToManyField(Question)
+    text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.text
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    chocies = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
